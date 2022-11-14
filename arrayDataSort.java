057ff9c87268b1c391cecd89f343f9a0f1d7cff4
KumarGangesh123 <116815600+KumarGangesh123@users.noreply.github.com>
2022-11-13 21:01:43 -0800

    Update arrayDataSort.java

--- arrayDataSort.java
+++ arrayDataSort.java
@@ -1,11 +1,15 @@
-/* this is the "arraySort.java" file in which the sorting operation has written */
+/* this is the "arrayDataSort.java" file in which the sorting operation has written */
 
-
+/* first of all you have complier the "arrays.java" file to create a package "array" then after 
+    compile ->  javac -d . arrayDataSort.java
+    run ->  java array.arrayDataInsert
+    
+*/
 
 
 package array;
 import java.util.Scanner;
-public class arraySort{
+public class arrayDataSort{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the range :\t");
